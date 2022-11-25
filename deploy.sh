254b5d5b4684ca8e70dab1af8c52c52400571e62
tamharo <117295948+tamharo@users.noreply.github.com>
2022-11-25 17:27:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ ZONE=europe-west1-b
 gcloud compute instances create $MY_INSTANCE_NAME \
     --image-family=debian-10 \
     --image-project=debian-cloud \
-    --project=goliv-c7734 
+    --project=goliv-c7734 \
     --machine-type=e2-medium \
     --scopes userinfo-email,cloud-platform \
     --metadata-from-file startup-script=startup.sh \
-    --zone europe-west1-b \
+    --zone $ZONE \
     --tags http-server
 
 gcloud compute firewall-rules create default-allow-http-8080 \
