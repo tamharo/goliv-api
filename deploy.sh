638abd7c3740b475bbba8d08832ca1dfa5625971
tamharo <117295948+tamharo@users.noreply.github.com>
2022-11-25 13:47:57 +0100

    update conf

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,9 @@ gcloud compute instances create $MY_INSTANCE_NAME \
     --metadata-from-file startup-script=startup.sh \
     --zone $ZONE \
     --tags http-server
+
+gcloud compute firewall-rules create default-allow-http-8080 \
+    --allow tcp:8080 \
+    --source-ranges 0.0.0.0/0 \
+    --target-tags http-server \
+    --description "Allow port 8080 access to http-server"
--- startup.sh
+++ startup.sh
@@ -5,22 +5,22 @@ pip install --upgrade pip virtualenv
 
 # Fetch source code
 export HOME=/root
-git clone  /opt/app
+git clone https://github.com/tamharo/goliv-api.git  /opt/app/search
 
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-virtualenv -p python3 env
-/bin/bash -c "source env/bin/activate"
-env/bin/pip install -r requirements.txt
+virtualenv -p python3 /opt/app/search/env
+/bin/bash -c "source /opt/app/search/env/bin/activate"
+/opt/app/search/env/bin/pip install -r /opt/app/search/requirements.txt
 
 # Set ownership to newly created account
-chown -R pythonapp:pythonapp /
+chown -R pythonapp:pythonapp /opt/app/search
 
 # Put supervisor configuration in proper place
-cp python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/search/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
