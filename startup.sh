b40d9eb7dbe42e98a6cd031cc89d4339b7629e71
tamharo <117295948+tamharo@users.noreply.github.com>
2022-11-25 18:00:29 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,24 +4,10 @@ sudo apt-get install -yq git supervisor python python-pip python3-distutils
 sudo pip install --upgrade pip virtualenv
 
 # Fetch source code
-export HOME=/root
+sudo export HOME=/root
 sudo git clone https://github.com/tamharo/goliv-api.git  /opt/app/search
 
-
-# Account to own server process
-useradd -m -d /home/pythonapp pythonapp
-
 # Python environment setup
-virtualenv -p python3 /opt/app/search/env
-/bin/bash -c "source /opt/app/search/env/bin/activate"
-/opt/app/search/env/bin/pip install -r /opt/app/search/requirements.txt
-
-# Set ownership to newly created account
-chown -R pythonapp:pythonapp /opt/app/search
-
-# Put supervisor configuration in proper place
-cp /opt/app/search/python-app.conf /etc/supervisor/conf.d/python-app.conf
-
-# Start service via supervisorctl
-supervisorctl reread
-supervisorctl update
+sudo virtualenv -p python3 /opt/app/search/env
+sudo /bin/bash -c "source /opt/app/search/env/bin/activate"
+sudo /opt/app/search/env/bin/pip install -r /opt/app/search/requirements.txt
