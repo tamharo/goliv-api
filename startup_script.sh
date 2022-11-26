3eac19b6a811b7174a607cc47fad7bbe696e57cb
tamharo <117295948+tamharo@users.noreply.github.com>
2022-11-26 15:10:50 +0100

    update folder save

--- startup_script.sh
+++ startup_script.sh
@@ -7,24 +7,24 @@ pip install --upgrade pip virtualenv
 
 # Fetch source code
 export HOME=/root
-git clone https://github.com/tamharo/goliv-api.git  /opt/app/search
+git clone https://github.com/tamharo/goliv-api.git  /opt/app/goliv
 
 # Install Cloud Ops Agent
-sudo bash /opt/app/search/add-google-cloud-ops-agent-repo.sh --also-install
+sudo bash /opt/app/goliv/add-google-cloud-ops-agent-repo.sh --also-install
 
 # Account to own server process
 useradd -m -d /home/pythonapp pythonapp
 
 # Python environment setup
-sudo virtualenv -p python3 /opt/app/search/env
-sudo /bin/bash -c "source /opt/app/search/env/bin/activate"
-sudo /opt/app/search/env/bin/pip install -r /opt/app/search/requirements.txt
+sudo virtualenv -p python3 /opt/app/goliv/env
+sudo /bin/bash -c "source /opt/app/goliv/env/bin/activate"
+sudo /opt/app/goliv/env/bin/pip install -r /opt/app/goliv/requirements.txt
 
 # Set ownership to newly created account
 chown -R pythonapp:pythonapp /opt/app
 
 # Put supervisor configuration in proper place
-cp /opt/app/search/python-app.conf /etc/supervisor/conf.d/python-app.conf
+cp /opt/app/goliv/python-app.conf /etc/supervisor/conf.d/python-app.conf
 
 # Start service via supervisorctl
 supervisorctl reread
