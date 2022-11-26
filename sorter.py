d5b5a54fabd176f47683f346921b86765ee560d3
tamharo <117295948+tamharo@users.noreply.github.com>
2022-11-26 16:56:36 +0100

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -55,7 +55,8 @@ def sorter():
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.solution_limit = 1
     search_parameters.time_limit.seconds = 120
     search_parameters.log_search = True
 
