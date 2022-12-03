a2beea378a9be9ba1a60282a569e93c14fab0b48
tamharo <117295948+tamharo@users.noreply.github.com>
2022-12-03 14:18:59 +0100

    update search method add open hdf5 outside function (global open file)

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from flask import Flask, request
 
 import json
 
-from search2 import getPlace
+from search3 import getPlace
 from sorter import sorter
 
-#import time
+import vaex
+
+import time
 
 app = Flask(__name__)
+
+start_time2 = time.time()
+
+print("--- %s seconds before open ---" % (time.time() - start_time2))
+dv = vaex.open(r'db/road.csv.hdf5')
+print("--- %s seconds after open ---" % (time.time() - start_time2))
 
 @app.route('/sorter', methods=['GET'])
 def api_sorter():
@@ -17,7 +25,7 @@ def api_sorter():
 def api_search():
     #start_time = time.time()
     z = request.args['city'] if request.args['city'] != '' else 'road'
-    search = getPlace(z, request.args['search'].upper())
+    search = getPlace(dv, request.args['search'].upper())
     #print("--- %s seconds ---" % (time.time() - start_time))
     return search
 
@@ -28,3 +36,5 @@ def say_hello():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+#127.0.0.1:8080/search?search=rue%20de%20franchepre&city=54
--- search3.py
+++ search3.py
@@ -9,6 +9,11 @@ import time
 
 from spelling import speelingAbbre
 
+
+
+
+
+
 def formatReg(search):
 
     formatedReg = search
@@ -20,22 +25,14 @@ def formatReg(search):
     return formatedReg
 
 
-def getPlace(search):
+def getPlace(dv, search):
     start_time = time.time()
     reg = formatReg(search)
-    print("--- %s seconds format reg ---" % (time.time() - start_time))
-    dv = vaex.open(r'db/road.csv.hdf5')
-    print("--- %s seconds open file ---" % (time.time() - start_time))
-    val = dv[dv.dept == "54" and dv.rue.str.contains("RUE DE FRANCHEPRE", regex=True)]
-    print("--- %s seconds search into file ---" % (time.time() - start_time))
-    result = val.rue.tolist()
-    #print(val)
-    print("--- %s seconds convert to list ---" % (time.time() - start_time))
     
-    
+    val = dv[dv.rue.str.contains(search, regex=True)]
     
     return val.rue.tolist()
 
-getPlace('RUE DE FRANCHE')
+
 
 #python3 search3.py
